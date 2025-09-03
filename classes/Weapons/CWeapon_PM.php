a392b6cec043a8e62bd9a5157efed9a3c52942ca
Chaek08 <chaek777zz@gmail.com>
2025-09-03 03:03:15 +0300

    фикс сборки

--- classes/Weapons/CWeapon_PM.php
+++ classes/Weapons/CWeapon_PM.php
@@ -1,8 +1,9 @@
 <?php 
 namespace app\forms\classes\Weapons;
+use app\forms\classes\Weapons\CWeapon;
 use app\forms\classes\CWeapon;
 
-class CWeapon extends CWeapon
+class CWeapon_PM extends CWeapon
 {
     
 }
